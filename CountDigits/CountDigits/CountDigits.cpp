e2b371af7381e69fc4522416fd3558a44cb2a6d9
Yaroslav Trofimov <159535808+CoR0NaExTrA@users.noreply.github.com>
2025-03-02 20:34:14 +0300

    Задание 5.3 Число цифр

---
+++ CountDigits/CountDigits/CountDigits.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <fstream>
+#include <cmath>
+
+long long countDigits(long long x) {
+    if (x == 0) return 0;
+    long long count = 0;
+    long long power = 1;
+    int length = 1;
+
+    while (power * 10 <= x) {
+        count += (power * 9) * length;
+        power *= 10;
+        length++;
+    }
+    count += (x - power + 1) * length;
+    return count;
+}
+
+int main() {
+    std::ifstream fin("INPUT.TXT");
+    std::ofstream fout("OUTPUT.TXT");
+
+    long long M, N;
+    fin >> M >> N;
+
+    long long result = countDigits(N) - countDigits(M - 1);
+    fout << result << std::endl;
+
+    return 0;
+}
