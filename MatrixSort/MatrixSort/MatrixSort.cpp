13f11a655f7a5d49becf805ecb332224405ee4fd
Yaroslav Trofimov <159535808+CoR0NaExTrA@users.noreply.github.com>
2025-03-10 09:27:27 +0300

    Ускорил производительность программу

--- MatrixSort/MatrixSort/MatrixSort.cpp
+++ MatrixSort/MatrixSort/MatrixSort.cpp
@@ -1,84 +1,90 @@
 ﻿#include <iostream>
 #include <fstream>
 #include <vector>
-#include <algorithm>
-#include <map>
+#include <bitset>
 
 using namespace std;
 
+const int MAX_N = 1000;
+
+// Функция для подсчёта минимальных перестановок
 void find_swaps(vector<int>& mapping, int& swaps) {
     int n = mapping.size();
-    vector<bool> visited(n, false);
+    bitset<MAX_N> visited;
 
     for (int i = 0; i < n; i++) {
         if (!visited[i]) {
-            int cycle_size = 0;
-            int j = i;
+            int cycle_size = 0, j = i;
             while (!visited[j]) {
-                visited[j] = true;
+                visited[j] = 1;
                 j = mapping[j];
                 cycle_size++;
             }
-            if (cycle_size > 1) {
-                swaps += (cycle_size - 1);
-            }
+            if (cycle_size > 1) swaps += (cycle_size - 1);
         }
     }
 }
 
 int main() {
-    ifstream fin("input3.txt");
+    ifstream fin("input2.txt");
     ofstream fout("OUTPUT.TXT");
 
     int n;
     fin >> n;
 
     vector<vector<int>> matrix(n, vector<int>(n));
-    map<int, pair<int, int>> positions;
+    vector<pair<int, int>> positions(n * n + 1);
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0, value; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            fin >> matrix[i][j];
-            positions[matrix[i][j]] = { i, j };
+            fin >> value;
+            positions[value] = { i, j };
         }
     }
 
-    vector<int> row_map(n), col_map(n);
+    vector<int> row_map(n, -1), col_map(n, -1);
+    bool is_possible = true;
 
     for (int i = 1; i <= n * n; i++) {
         int sorted_row = (i - 1) / n;
         int sorted_col = (i - 1) % n;
-
         auto [cur_row, cur_col] = positions[i];
 
-        row_map[cur_row] = sorted_row;
-        col_map[cur_col] = sorted_col;
-    }
+        if (row_map[cur_row] == -1) {
+            row_map[cur_row] = sorted_row;
+        }
+        else if (row_map[cur_row] != sorted_row) {
+            is_possible = false;
+            break;
+        }
 
-    sort(row_map.begin(), row_map.end());
-    sort(col_map.begin(), col_map.end());
-
-    for (int i = 0; i < n; i++) {
-        if (row_map[i] != i || col_map[i] != i) {
-            fout << "No" << endl;
-            return 0;
+        if (col_map[cur_col] == -1) {
+            col_map[cur_col] = sorted_col;
+        }
+        else if (col_map[cur_col] != sorted_col) {
+            is_possible = false;
+            break;
         }
     }
 
-    fout << "Yes" << endl;
+    if (!is_possible) {
+        fout << "No\n";
+        return 0;
+    }
+
+    fout << "Yes\n";
 
     vector<int> row_permutation(n), col_permutation(n);
-
     for (int i = 0; i < n; i++) {
-        row_permutation[positions[i * n + 1].first] = i;
-        col_permutation[positions[i + 1].second] = i;
+        row_permutation[row_map[i]] = i;
+        col_permutation[col_map[i]] = i;
     }
 
     int swaps = 0;
     find_swaps(row_permutation, swaps);
     find_swaps(col_permutation, swaps);
 
-    fout << swaps << endl;
+    fout << swaps << '\n';
 
     return 0;
 }
